91e05177323abb31dc55844d01c184b4796bb424
recyno <rezacahyono1@gmail.com>
2022-03-20 21:11:35 +0700

    add image dice & unit test

--- DiceRoller/app/src/main/java/com/rchyno/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/rchyno/diceroller/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.rchyno.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rchyno.diceroller.databinding.ActivityMainBinding
+import com.rchyno.diceroller.model.Dice
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRoll.setOnClickListener { diceRoll() }
     }
+
+    private fun diceRoll() {
+        val dice = Dice(6)
+        val diceImageResource = when(dice.roll()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.ivDice.setImageResource(diceImageResource)
+    }
+
 }
--- DiceRoller/app/src/test/java/com/rchyno/diceroller/ExampleUnitTest.kt
+++ DiceRoller/app/src/test/java/com/rchyno/diceroller/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.rchyno.diceroller
 
+import com.rchyno.diceroller.model.Dice
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,12 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun rollDice(){
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("the value of rollResult was not berween 1 and 6", rollResult in 1..6)
     }
 }
