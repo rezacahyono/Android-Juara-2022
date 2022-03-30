2fc43b315ed62a8c3e1c8ef8537a8d222cd61fd5
rezacahyono <rezacahyono1@gmail.com>
2022-03-30 15:44:37 +0700

    🚧clean-code

--- android/TipTime/app/src/main/java/com/rezac/tiptime/MainActivity.kt
+++ android/TipTime/app/src/main/java/com/rezac/tiptime/MainActivity.kt
@@ -2,10 +2,42 @@ package com.rezac.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rezac.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfArea.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        if (cost == null || cost == 0.0){
+            displayTip(0.0)
+            return
+        }
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+        displayTip(tip)
+    }
+
+
+    private fun displayTip(tip: Double){
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = resources.getString(R.string.tip_amount, formattedTip)
     }
 }
