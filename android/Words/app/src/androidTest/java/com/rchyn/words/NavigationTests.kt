1751fdb0b33f5b49410279327d387bf07f85de17
rezacahyono <rezacahyono1@gmail.com>
2022-04-04 03:03:01 +0700

    Words: Adding Instrument Testing Navigation

---
+++ android/Words/app/src/androidTest/java/com/rchyn/words/NavigationTests.kt
@@ -0,0 +1,54 @@
+package com.rchyn.words
+
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragment
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.rchyn.words.ui.fragment.LetterListFragment
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTests {
+
+    private lateinit var navController: TestNavHostController
+    private lateinit var letterListScenario: FragmentScenario<LetterListFragment>
+
+    @Before
+    fun setUp() {
+        navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        
+        letterListScenario =
+            launchFragmentInContainer(themeResId = R.style.Theme_Words)
+
+        letterListScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+    }
+
+    @Test
+    fun navigation_to_words_nav_component() {
+        onView(withId(R.id.recycler_letter)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                2,
+                click()
+            )
+        )
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+    }
+
+}
