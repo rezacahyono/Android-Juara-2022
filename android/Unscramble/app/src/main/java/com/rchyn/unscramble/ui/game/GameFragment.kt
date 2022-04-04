80422de4351ce35c58b7e2dd745eb8d0f3df5d1b
rezacahyono <rezacahyono1@gmail.com>
2022-04-04 21:42:03 +0700

    merge-data-binding-implement-livedata

--- android/Unscramble/app/src/main/java/com/rchyn/unscramble/ui/game/GameFragment.kt
+++ android/Unscramble/app/src/main/java/com/rchyn/unscramble/ui/game/GameFragment.kt
@@ -1,36 +1,126 @@
 package com.rchyn.unscramble.ui.game
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.rchyn.unscramble.R
 import com.rchyn.unscramble.databinding.FragmentGameBinding
 
-
 class GameFragment : Fragment() {
 
-    private var _binding: FragmentGameBinding? = null
-    private val binding get() = _binding!!
+    private val viewModel: GameViewModel by viewModels()
+
+    private lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentGameBinding.inflate(layoutInflater, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_game, container, false)
+        Log.d("GameFragment", "GameFragment created/re-created!")
+        Log.d(
+            "GameFragment", "Word: ${viewModel.currentScrambledWord} " +
+                    "Score: ${viewModel.score} WordCount: ${viewModel.currentWordCount}"
+        )
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.gameViewModel = viewModel
+        binding.maxNoOfWords = MAX_NO_OF_WORDS
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.submit.setOnClickListener { onSubmitWord() }
+        binding.skip.setOnClickListener { onSkipWord() }
     }
 
+    /**
+     * Checks the user's word, and updates the score accordingly.
+     * Displays the next scrambled word.
+     * After the last word, the user is shown a Dialog with the final score.
+     */
+    private fun onSubmitWord() {
+        val playerWord = binding.textInputEditText.text.toString()
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        if (viewModel.isUserWordCorrect(playerWord)) {
+            setErrorTextField(false)
+            if (!viewModel.nextWord()) {
+                showFinalScoreDialog()
+            }
+        } else {
+            setErrorTextField(true)
+        }
+    }
+
+    /**
+     * Skips the current word without changing the score.
+     */
+    private fun onSkipWord() {
+        if (viewModel.nextWord()) {
+            setErrorTextField(false)
+        } else {
+            showFinalScoreDialog()
+        }
+    }
+
+    /**
+     * Creates and shows an AlertDialog with the final score.
+     */
+    private fun showFinalScoreDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.congratulations))
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
+            .setCancelable(false)
+            .setNegativeButton(getString(R.string.exit)) { _, _ ->
+                exitGame()
+            }
+            .setPositiveButton(getString(R.string.play_again)) { _, _ ->
+                restartGame()
+            }
+            .show()
+    }
+
+    /**
+     * Re-initializes the data in the ViewModel and updates the views with the new data, to
+     * restart the game.
+     */
+    private fun restartGame() {
+        viewModel.reinitializeData()
+        setErrorTextField(false)
+    }
+
+    /**
+     * Exits the game.
+     */
+    private fun exitGame() {
+        activity?.finish()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d("GameFragment", "GameFragment destroyed!")
+    }
+
+    /**
+     * Sets and resets the text field error status.
+     */
+    private fun setErrorTextField(error: Boolean) {
+        if (error) {
+            binding.textField.isErrorEnabled = true
+            binding.textField.error = getString(R.string.try_again)
+        } else {
+            binding.textField.isErrorEnabled = false
+            binding.textInputEditText.text = null
+        }
     }
 
 }
